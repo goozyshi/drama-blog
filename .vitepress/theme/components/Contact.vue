1ddf216537748850563807523714a850a2f52a21
goozyshi <goozyshi@gmail.com>
2024-03-27 21:30:20 +0800

    style: adjust

--- .vitepress/theme/components/Contact.vue
+++ .vitepress/theme/components/Contact.vue
@@ -4,28 +4,29 @@
 <template>
   <h4 class="font-semibold self-start" >Contact Me:</h4>
   <div class="flex self-start items-start justify-start flex-wrap">
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/goozyshi-fff?logo=wechat"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/goozyshi@gmail.com-fff?logo=Gmail"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/goozyshi-fff?logo=wechat"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/goozyshi@gmail.com-fff?logo=Gmail"/>
   </div>
   <h4 class="font-semibold self-start" >Languages:</h4>
   <div class="flex self-start items-start justify-start flex-wrap">
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/HTML5-E34F26?logo=HTML5&logoColor=fff"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/CSS3-1572B6?logo=CSS3&logoColor=fff"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/JavaScript-F7DF1E?logo=JavaScript&logoColor=333"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/Node.js-417038?logo=Node.js&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/HTML5-E34F26?logo=HTML5&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/CSS3-1572B6?logo=CSS3&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/JavaScript-F7DF1E?logo=JavaScript&logoColor=333"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/Node.js-417038?logo=Node.js&logoColor=fff"/>
   </div>
   <h4 class="font-semibold self-start" >Frameworks:</h4>
   <div class="flex self-start items-start justify-start flex-wrap">
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/Vue-42b883?logo=vue.js&logoColor=fff"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/Nuxt-000?logo=nuxt.js&logoColor=00dc82"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/React-fff?logo=react&logoColor=087ea4"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/Sass-CC6699?logo=Sass&logoColor=fff"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/TailwindCSS-fff?logo=tailwindcss&logoColor=0ea5e9"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/Vue-42b883?logo=vue.js&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/Nuxt-333?logo=nuxt.js&logoColor=00dc82"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/React-087ea4?logo=react&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/Sass-CC6699?logo=Sass&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/TailwindCSS-0ea5e9?logo=tailwindcss&logoColor=fff"/>
   </div>
   <h4 class="font-semibold self-start" >Tools:</h4>
   <div class="flex self-start items-start justify-start flex-wrap">
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/VS%20CODE-007ACC?logo=VisualStudioCode&logoColor=fff"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/sublime%20text-555?logo=sublimetext&logoColor=f09642"/>
-    <img class="mx-1 py-2" src="https://img.shields.io/badge/Git-F05032?logo=Git&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/VS%20CODE-007ACC?logo=VisualStudioCode&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/sublime%20text-555?logo=sublimetext&logoColor=f09642"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/Git-F05032?logo=Git&logoColor=fff"/>
+    <img class="px-1 py-1" src="https://img.shields.io/badge/markdown-999?logo=markdown&logoColor=fff"/>
   </div>
 </template>
